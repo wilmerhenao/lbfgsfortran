b57e8b24ecb12fc0bd202a7226ef898fea16e4c1
Wilmer Henao <Wilmer.Henao@nyu.edu>
2013-06-11 07:04:33 -0400

    added new function line search

--- blas.f
+++ blas.f
@@ -122,10 +122,10 @@ c
 c
       if(n.le.0)return
       if(incx.eq.1.and.incy.eq.1)go to 20
-c
-c        code for unequal increments or equal increments
-c          not equal to 1
-c
+
+c     code for unequal increments or equal increments
+c     not equal to 1
+
       ix = 1
       iy = 1
       if(incx.lt.0)ix = (-n+1)*incx + 1
@@ -136,12 +136,12 @@ c
         iy = iy + incy
    10 continue
       return
-c
+
 c        code for both increments equal to 1
-c
-c
+
+
 c        clean-up loop
-c
+
    20 m = mod(n,7)
       if( m .eq. 0 ) go to 40
       do 30 i = 1,m
@@ -150,14 +150,14 @@ c
       if( n .lt. 7 ) return
    40 mp1 = m + 1
       do 50 i = mp1,n,7
-        dy(i) = dx(i)
-        dy(i + 1) = dx(i + 1)
-        dy(i + 2) = dx(i + 2)
-        dy(i + 3) = dx(i + 3)
-        dy(i + 4) = dx(i + 4)
-        dy(i + 5) = dx(i + 5)
-        dy(i + 6) = dx(i + 6)
-   50 continue
+         dy(i) = dx(i)
+         dy(i + 1) = dx(i + 1)
+         dy(i + 2) = dx(i + 2)
+         dy(i + 3) = dx(i + 3)
+         dy(i + 4) = dx(i + 4)
+         dy(i + 5) = dx(i + 5)
+         dy(i + 6) = dx(i + 6)
+ 50   continue
       return
       end
       
--- lbfgsb.f
+++ lbfgsb.f
@@ -2569,7 +2569,7 @@ c                               Line search is impossible.
          else
             do 41 i = 1, n
                x(i) = stp*d(i) + t(i)
-  41        continue
+ 41         continue
          endif
       else
          task = 'NEW_X'
@@ -3351,7 +3351,6 @@ c====================== The end of subsm ===============================
       double precision f,g,stp,ftol,gtol,xtol,stpmin,stpmax
       double precision dsave(13)
 c     **********
-c
 c     Subroutine dcsrch
 c
 c     This subroutine finds a step that satisfies a sufficient
@@ -3401,19 +3400,19 @@ c        subroutine dcsrch(f,g,stp,ftol,gtol,xtol,stpmin,stpmax,
 c                          task,isave,dsave)
 c     where
 c
-c       f is a double precision variable.
+c       f    is a double precision variable.
 c         On initial entry f is the value of the function at 0.
 c            On subsequent entries f is the value of the 
 c            function at stp.
 c         On exit f is the value of the function at stp.
 c
-c       g is a double precision variable.
+c       g    is a double precision variable.
 c         On initial entry g is the derivative of the function at 0.
 c            On subsequent entries g is the derivative of the 
 c            function at stp.
 c         On exit g is the derivative of the function at stp.
 c
-c       stp is a double precision variable. 
+c       stp  is a double precision variable. 
 c         On entry stp is the current estimate of a satisfactory 
 c            step. On initial entry, a positive initial estimate 
 c            must be provided. 
@@ -3514,11 +3513,11 @@ c        Exit if there are errors on input.
 c        Initialize local variables.
 
          brackt = .false.
-         stage = 1
-         finit = f
-         ginit = g
-         gtest = ftol*ginit
-         width = stpmax - stpmin
+         stage  = 1
+         finit  = f
+         ginit  = g
+         gtest  = ftol*ginit
+         width  = stpmax - stpmin
          width1 = width/p5
 
 c        The variables stx, fx, gx contain the values of the step, 
@@ -3537,13 +3536,13 @@ c        function, and derivative at stp.
          stmin = zero
          stmax = stp + xtrapu*stp
          task = 'FG'
-
+         
          goto 1000
-
+         
       else
-
+         
 c     Restore local variables.
-
+         
          if (isave(1) .eq. 1) then
             brackt = .true.
          else
@@ -3571,7 +3570,7 @@ c     algorithm enters the second stage.
 
       ftest = finit + stp * gtest 
       if (stage .eq. 1 .and. f .le. ftest .and. g .ge. zero) 
-     +   stage = 2
+     +     stage = 2
 
 c     Test for warnings.
 
@@ -3615,7 +3614,7 @@ c        Call dcstep to update stx, sty, and to compute the new step.
          call dcstep(stx,fxm,gxm,sty,fym,gym,stp,fm,gm,
      +               brackt,stmin,stmax)
 
-c        Reset the function and derivative values for f.
+c        Reset the function and derivative values for f
 
          fx = fxm + stx*gtest
          fy = fym + sty*gtest
@@ -3949,3 +3948,262 @@ c     Compute the new step.
       return
       end
       
+c====================== The end of dcstep ==============================
+
+      subroutine linesearchstep(stx,fx,dx,sty,fy,dy,stp,fp,dp,brackt,
+     +                  stpmin,stpmax)
+      logical brackt
+      double precision stx,fx,dx,sty,fy,dy,stp,fp,dp,stpmin,stpmax
+c     **********
+c
+c     Subroutine linesearchstep
+c
+c     This subroutine computes a safeguarded step for a search
+c     procedure and updates an interval that contains a step that
+c     satisfies a sufficient decrease and a curvature condition.
+c
+c     The parameter stx contains the step with the least function
+c     value. If brackt is set to .true. then a minimizer has
+c     been bracketed in an interval with endpoints stx and sty.
+c     The parameter stp contains the current step. 
+c     The subroutine assumes that if brackt is set to .true. then
+c
+c           min(stx,sty) < stp < max(stx,sty),
+c
+c     and that the derivative at stx is negative in the direction 
+c     of the step.
+c
+c     The subroutine statement is
+c
+c       subroutine linesearchstep(stx,fx,dx,sty,fy,dy,stp,fp,dp,brackt,
+c                         stpmin,stpmax)
+c
+c     where
+c
+c       stx is a double precision variable.
+c         On entry stx is the best step obtained so far and is an
+c            endpoint of the interval that contains the minimizer. 
+c         On exit stx is the updated best step.
+c
+c       fx is a double precision variable.
+c         On entry fx is the function at stx.
+c         On exit fx is the function at stx.
+c
+c       dx is a double precision variable.
+c         On entry dx is the derivative of the function at 
+c            stx. The derivative must be negative in the direction of 
+c            the step, that is, dx and stp - stx must have opposite 
+c            signs.
+c         On exit dx is the derivative of the function at stx.
+c
+c       sty is a double precision variable.
+c         On entry sty is the second endpoint of the interval that 
+c            contains the minimizer.
+c         On exit sty is the updated endpoint of the interval that 
+c            contains the minimizer.
+c
+c       fy is a double precision variable.
+c         On entry fy is the function at sty.
+c         On exit fy is the function at sty.
+c
+c       dy is a double precision variable.
+c         On entry dy is the derivative of the function at sty.
+c         On exit dy is the derivative of the function at the exit sty.
+c
+c       stp is a double precision variable.
+c         On entry stp is the current step. If brackt is set to .true.
+c            then on input stp must be between stx and sty. 
+c         On exit stp is a new trial step.
+c
+c       fp is a double precision variable.
+c         On entry fp is the function at stp
+c         On exit fp is unchanged.
+c
+c       dp is a double precision variable.
+c         On entry dp is the the derivative of the function at stp.
+c         On exit dp is unchanged.
+c
+c       brackt is an logical variable.
+c         On entry brackt specifies if a minimizer has been bracketed.
+c            Initially brackt must be set to .false.
+c         On exit brackt specifies if a minimizer has been bracketed.
+c            When a minimizer is bracketed brackt is set to .true.
+c
+c       stpmin is a double precision variable.
+c         On entry stpmin is a lower bound for the step.
+c         On exit stpmin is unchanged.
+c
+c       stpmax is a double precision variable.
+c         On entry stpmax is an upper bound for the step.
+c         On exit stpmax is unchanged.
+c
+c     MINPACK-1 Project. June 1983
+c     Argonne National Laboratory. 
+c     Jorge J. More' and David J. Thuente.
+c     
+c     MINPACK-2 Project. October 1993.
+c     Argonne National Laboratory and University of Minnesota. 
+c     Brett M. Averick and Jorge J. More'.
+c     
+c     **********
+      double precision zero,p66,two,three
+      parameter(zero=0.0d0,p66=0.66d0,two=2.0d0,three=3.0d0)
+      
+      double precision gamma,p,q,r,s,sgnd,stpc,stpf,stpq,theta
+      
+      sgnd = dp*(dx/abs(dx))
+      
+c     First case: A higher function value. The minimum is bracketed. 
+c     If the cubic step is closer to stx than the quadratic step, the 
+c     cubic step is taken, otherwise the average of the cubic and 
+c     quadratic steps is taken.
+      
+      if (fp .gt. fx) then
+         theta = three*(fx - fp)/(stp - stx) + dx + dp
+         s = max(abs(theta),abs(dx),abs(dp))
+         gamma = s*sqrt((theta/s)**2 - (dx/s)*(dp/s))
+         if (stp .lt. stx) gamma = -gamma
+         p = (gamma - dx) + theta
+         q = ((gamma - dx) + gamma) + dp
+         r = p/q
+         stpc = stx + r*(stp - stx)
+         stpq = stx + ((dx/((fx - fp)/(stp - stx) + dx))/two)*
+     +                                                       (stp - stx)
+         if (abs(stpc-stx) .lt. abs(stpq-stx)) then
+            stpf = stpc
+         else
+            stpf = stpc + (stpq - stpc)/two
+         endif
+         brackt = .true.
+
+c     Second case: A lower function value and derivatives of opposite 
+c     sign. The minimum is bracketed. If the cubic step is farther from 
+c     stp than the secant step, the cubic step is taken, otherwise the 
+c     secant step is taken.
+
+      else if (sgnd .lt. zero) then
+         theta = three*(fx - fp)/(stp - stx) + dx + dp
+         s = max(abs(theta),abs(dx),abs(dp))
+         gamma = s*sqrt((theta/s)**2 - (dx/s)*(dp/s))
+         if (stp .gt. stx) gamma = -gamma
+         p = (gamma - dp) + theta
+         q = ((gamma - dp) + gamma) + dx
+         r = p/q
+         stpc = stp + r*(stx - stp)
+         stpq = stp + (dp/(dp - dx))*(stx - stp)
+         if (abs(stpc-stp) .gt. abs(stpq-stp)) then
+            stpf = stpc
+         else
+            stpf = stpq
+         endif
+         brackt = .true.
+
+c     Third case: A lower function value, derivatives of the same sign,
+c     and the magnitude of the derivative decreases.
+
+      else if (abs(dp) .lt. abs(dx)) then
+
+c        The cubic step is computed only if the cubic tends to infinity 
+c        in the direction of the step or if the minimum of the cubic
+c        is beyond stp. Otherwise the cubic step is defined to be the 
+c        secant step.
+
+         theta = three*(fx - fp)/(stp - stx) + dx + dp
+         s = max(abs(theta),abs(dx),abs(dp))
+
+c        The case gamma = 0 only arises if the cubic does not tend
+c        to infinity in the direction of the step.
+
+         gamma = s*sqrt(max(zero,(theta/s)**2-(dx/s)*(dp/s)))
+         if (stp .gt. stx) gamma = -gamma
+         p = (gamma - dp) + theta
+         q = (gamma + (dx - dp)) + gamma
+         r = p/q
+         if (r .lt. zero .and. gamma .ne. zero) then
+            stpc = stp + r*(stx - stp)
+         else if (stp .gt. stx) then
+            stpc = stpmax
+         else
+            stpc = stpmin
+         endif
+         stpq = stp + (dp/(dp - dx))*(stx - stp)
+
+         if (brackt) then
+
+c           A minimizer has been bracketed. If the cubic step is 
+c           closer to stp than the secant step, the cubic step is 
+c           taken, otherwise the secant step is taken.
+
+            if (abs(stpc-stp) .lt. abs(stpq-stp)) then
+               stpf = stpc
+            else
+               stpf = stpq
+            endif
+            if (stp .gt. stx) then
+               stpf = min(stp+p66*(sty-stp),stpf)
+            else
+               stpf = max(stp+p66*(sty-stp),stpf)
+            endif
+         else
+
+c           A minimizer has not been bracketed. If the cubic step is 
+c           farther from stp than the secant step, the cubic step is 
+c           taken, otherwise the secant step is taken.
+
+            if (abs(stpc-stp) .gt. abs(stpq-stp)) then
+               stpf = stpc
+            else
+               stpf = stpq
+            endif
+            stpf = min(stpmax,stpf)
+            stpf = max(stpmin,stpf)
+         endif
+
+c     Fourth case: A lower function value, derivatives of the same sign, 
+c     and the magnitude of the derivative does not decrease. If the 
+c     minimum is not bracketed, the step is either stpmin or stpmax, 
+c     otherwise the cubic step is taken.
+
+      else
+         if (brackt) then
+            theta = three*(fp - fy)/(sty - stp) + dy + dp
+            s = max(abs(theta),abs(dy),abs(dp))
+            gamma = s*sqrt((theta/s)**2 - (dy/s)*(dp/s))
+            if (stp .gt. sty) gamma = -gamma
+            p = (gamma - dp) + theta
+            q = ((gamma - dp) + gamma) + dy
+            r = p/q
+            stpc = stp + r*(sty - stp)
+            stpf = stpc
+         else if (stp .gt. stx) then
+            stpf = stpmax
+         else
+            stpf = stpmin
+         endif
+      endif
+
+c     Update the interval which contains a minimizer.
+
+      if (fp .gt. fx) then
+         sty = stp
+         fy = fp
+         dy = dp
+      else
+         if (sgnd .lt. zero) then
+            sty = stx
+            fy = fx
+            dy = dx
+         endif
+         stx = stp
+         fx = fp
+         dx = dp
+      endif
+
+c     Compute the new step.
+
+      stp = stpf
+
+      return
+      end
+      
+c====================== The end of linesearchstep ==============================
