551f30aa0bd21d23bd35fd69f34b7d35f86aeebc
wilmer henao <weh227@nyu.edu>
2014-02-28 18:21:54 -0500

    Running m vs p

--- rosenbrockscriptgenerator.sh
+++ rosenbrockscriptgenerator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #for i in {2 1.1 1.01 1.001 1.0001 1.00001 1.000001 1.0000001 1.00000001 1.000000001 1.0000000001 1.00000000001 0.5}
-for i in {0..14}
+for i in {0..20}
 do
   cat > pbs.script.rosenbrock.$i <<EOF
 #!/bin/bash
